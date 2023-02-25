86e9a1518b87f61add41f0bed7ce4f8e1395d48a
Nguyen Nam Duong <DuongNNb@bkav.com>
2023-02-25 16:35:01 +0700

    Add-up

--- Mid-Term/Test06.py
+++ Mid-Term/Test06.py
@@ -1,15 +1,15 @@
 S = 0
 avr = 0
-money = []
+money = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 res = ""
-for i in range(1, 13):
+for i in range(0, 12):
     money[i] = int(input())
     S = S + money[i]
 avr = float(S / 12)
 print("Tổng:", S)
 print("TBC:", avr)
 print("Các tháng nhiều hơn TBC:")
-for i in range(1, 13):
+for i in range(0, 12):
     if money[i] > avr:
-        res = res + str(money[i]) + " "
+        res = res + str(i) + " "
 print(res)
